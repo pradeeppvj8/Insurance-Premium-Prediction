fd665f310603756dffd8489485f34162c530a1a5
pradeeppvj8 <pradeep.pvj8@gmail,com>
2023-09-18 20:07:14 +0530

    Updated config_entity and artifact_entity file

---
+++ ippredictor/entity/config_entity.py
@@ -0,0 +1,37 @@
+import os, sys
+from datetime import datetime
+from ippredictor.logger import logging
+from ippredictor.exception import IPPPredictorException
+
+FILE_NAME = "insurance.csv"
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        try:
+            self.artifacts_dir = os.path.join(os.getcwd(), "artifacts", f"{datetime.now().strftime('%m%d%Y__%H%M%S')}")
+        except Exception as e:
+            logging.error(e)
+            raise IPPPredictorException(e, sys)
+        
+class DataIngestionConfig:
+    def __init__(self, training_pipeline_config : TrainingPipelineConfig):
+        try:
+            self.database_name = "INSURANCE"
+            self.collection_name = "INSURANCE_DETAILS"
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifacts_dir, "data_ingestion")
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir, "feature_store", FILE_NAME)
+            self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset", TRAIN_FILE_NAME)
+            self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset", TEST_FILE_NAME)
+        except Exception as e:
+            logging.error(e)
+            raise IPPPredictorException(e, sys)
+        
+    def to_dict(self) -> dict:
+        try:
+            return self.__dict__
+        except Exception as e:
+            logging.error(e)
+            raise IPPPredictorException(e, sys)
+        
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from ippredictor.logger import logging
 from ippredictor.exception import IPPPredictorException
 import os, sys
 from ippredictor.utils import get_collection_as_data_frame
+from ippredictor.entity.config_entity import DataIngestionConfig
+from ippredictor.entity.config_entity import TrainingPipelineConfig
 
 def test_logger_and_exception():
     try:
@@ -15,4 +17,7 @@ def test_logger_and_exception():
     
 if __name__ == "__main__":
     #test_logger_and_exception()
-    get_collection_as_data_frame("INSURANCE","INSURANCE_DETAILS")
+    #get_collection_as_data_frame("INSURANCE","INSURANCE_DETAILS")
+    training_pipeline_config = TrainingPipelineConfig()
+    data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    logging.info(data_ingestion_config.to_dict())
